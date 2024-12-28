527b687a0143994f99d44a119b620557469ed550
ghulamkarimi <karimiamirhossein12@gmail.com>
2024-12-28 01:16:31 +0100

    Update CORS configuration to allow specific production origins for socket connections

--- socket/socket.js
+++ socket/socket.js
@@ -4,7 +4,7 @@ const initializeSocket = (server) => {
     const io = new Server(server, {
       cors: {
         // origin: ["http://localhost:3000", "http://localhost:5173", "https://ghulam-dev.me"],
-        origin: ["http://localhost:3000", "http://localhost:5173","http://localhost:7001"],
+        origin: ["https://car-db.aundoautoservice.de", "https://www.aundoautoservice.de", "https://admin.aundoautoservice.de"],
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         credentials: true,
       },
