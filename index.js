48e512b698fb95dea22561be393be747237c0c59
ghulamkarimi <karimiamirhossein12@gmail.com>
2024-12-29 05:32:43 +0100

    Refactor CORS configuration and improve code formatting in index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { dbConnect } from './dbConnect/dbConnect.js';
-import userRouter from './routes/userRouter.js';
-import carRentRouter from './routes/carRentRouter.js';
-import carBuyRouter from './routes/carBuyRouter.js';
-import offerRouter from './routes/offerRouter.js';
-import paymentRouter from './routes/paymentRouter.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import compression from 'compression';
-import schutzPacketRouter from './routes/schutzPacktRouter.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { createServer } from 'http';
-import initializeSocket from './socket/socket.js';
-import reservationRouter from './routes/reservationRouter.js';
-import appointmentRouter from './routes/appointmentRouter.js';
+import express from "express";
+import dotenv from "dotenv";
+import { dbConnect } from "./dbConnect/dbConnect.js";
+import userRouter from "./routes/userRouter.js";
+import carRentRouter from "./routes/carRentRouter.js";
+import carBuyRouter from "./routes/carBuyRouter.js";
+import offerRouter from "./routes/offerRouter.js";
+import paymentRouter from "./routes/paymentRouter.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import compression from "compression";
+import schutzPacketRouter from "./routes/schutzPacktRouter.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createServer } from "http";
+import initializeSocket from "./socket/socket.js";
+import reservationRouter from "./routes/reservationRouter.js";
+import appointmentRouter from "./routes/appointmentRouter.js";
 
 dotenv.config();
 dbConnect();
@@ -28,57 +28,59 @@ const server = createServer(app);
 const io = initializeSocket(server);
 
 app.use((req, res, next) => {
-    req.io = io;
-    next();
+  req.io = io;
+  next();
 });
 
 // Middleware setup
-app.use(express.json({ limit: '20mb' })); // Erhöht die maximale JSON-Größe
-app.use(express.urlencoded({ extended: true, limit: '20mb' })); // Für große Formulardaten
+app.use(express.json({ limit: "20mb" })); // Erhöht die maximale JSON-Größe
+app.use(express.urlencoded({ extended: true, limit: "20mb" })); // Für große Formulardaten
 app.use(compression());
 app.use(cookieParser());
 
-const allowedOrigins =
-process.env.NODE_ENV === "production"
-    ? ["https://car-db.aundoautoservice.de","https://aundoautoservice.de" ,"https://www.aundoautoservice.de", "https://admin.aundoautoservice.de"]
-   : []
-    app.use(
-      cors({
-        origin: (origin, callback) => {
-          if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-          } else {
-            console.error(`CORS blockiert: ${origin} ist nicht erlaubt`);
-            callback(new Error(`CORS blockiert: ${origin} ist nicht erlaubt`));
-          }
-        },
-        credentials: true,
-        methods: ["GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"],
-        allowedHeaders: [
-          "Content-Type",
-          "Authorization",
-          "Accept",
-          "Origin",
-          "X-Requested-With",
-        ],
-      })
-    );
-    
+const allowedOrigins = [
+  "https://car-db.aundoautoservice.de",
+  "https://aundoautoservice.de",
+  "https://www.aundoautoservice.de",
+  "https://admin.aundoautoservice.de",
+  
+];
 
-app.options('*', cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.error(`CORS blockiert: ${origin} ist nicht erlaubt`);
+        callback(new Error(`CORS blockiert: ${origin} ist nicht erlaubt`));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Accept",
+      "Origin",
+      "X-Requested-With",
+    ],
+  })
+);
 
+app.options("*", cors());
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 // Routen
-app.use('/user', userRouter);
-app.use('/rent', carRentRouter);
-app.use('/buy', carBuyRouter);
-app.use('/offer', offerRouter);
-app.use('/appointment', appointmentRouter);
-app.use('/payment', paymentRouter);
-app.use('/schutzPacket', schutzPacketRouter);
-app.use('/reservation', reservationRouter);
+app.use("/user", userRouter);
+app.use("/rent", carRentRouter);
+app.use("/buy", carBuyRouter);
+app.use("/offer", offerRouter);
+app.use("/appointment", appointmentRouter);
+app.use("/payment", paymentRouter);
+app.use("/schutzPacket", schutzPacketRouter);
+app.use("/reservation", reservationRouter);
 
 // Fehlerbehandlung
 app.use((err, req, res, next) => {
