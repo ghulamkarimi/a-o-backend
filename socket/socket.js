38564f39c6197f97638589b18ac678cde4cdd957
ghulamkarimi <karimiamirhossein12@gmail.com>
2025-01-11 08:53:48 +0100

    Update CORS configuration in socket.js to include additional allowed origin

--- socket/socket.js
+++ socket/socket.js
@@ -1,25 +1,29 @@
-import { Server } from "socket.io"; 
+import { Server } from "socket.io";
 
 const initializeSocket = (server) => {
-    const io = new Server(server, {
-      cors: {
-        // origin: ["http://localhost:3000", "http://localhost:5173", "https://ghulam-dev.me"],
-        origin: ["https://car-db.aundoautoservice.de", "https://www.aundoautoservice.de", "https://admin.aundoautoservice.de"],
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        credentials: true,
-      },
+  const io = new Server(server, {
+    cors: {
+      // origin: ["http://localhost:3000", "http://localhost:5173", "https://ghulam-dev.me"],
+      origin: [
+        "https://car-db.aundoautoservice.de",
+        "https://www.aundoautoservice.de",
+        "https://aundoautoservice.de",
+        "https://admin.aundoautoservice.de",
+      ],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      credentials: true,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("WebSocket verbunden:", socket.id);
+
+    socket.on("disconnect", () => {
+      console.log("WebSocket getrennt:", socket.id);
     });
-  
-    io.on("connection", (socket) => {
-      console.log("WebSocket verbunden:", socket.id);
-  
-      socket.on("disconnect", () => {
-        console.log("WebSocket getrennt:", socket.id);
-      });
-    });
-  
-    return io;
-  };
-  
+  });
+
+  return io;
+};
 
 export default initializeSocket;
